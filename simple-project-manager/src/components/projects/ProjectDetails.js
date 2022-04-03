8f2a97e6a872d8d88cfa937923c79dbcfe4fb140
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-03 14:01:57 -0500

    Created id variable to store the props.match.params.id for each project

--- simple-project-manager/src/components/projects/ProjectDetails.js
+++ simple-project-manager/src/components/projects/ProjectDetails.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-const ProjectDetails = () => {
+const ProjectDetails = (props) => {
+    const id = props.match.params.id;
     return (
         <div className="container section project-details">
             <div className="card z-depth-0">
                 <div className="card-content">
-                    <span className="card-title">Project Title</span>
+                    <span className="card-title">Project Title - {id}</span>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis blanditiis autem recusandae similique asperiores sed, ratione nemo ducimus voluptate explicabo aliquid, ipsa accusamus impedit consectetur quo accusantium vero praesentium numquam!</p>
                 </div>
                 <div className="card-action grey lighten-4 grey-text">
