14850305fdba51126c692cacf1deb34f7313afca
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-03 15:59:19 -0500

    Provider is wrapping the App component and passing in the store={store} to it

--- simple-project-manager/src/index.js
+++ simple-project-manager/src/index.js
@@ -6,12 +6,15 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
+import { Provider } from 'react-redux';
 
 const store = createStore();
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
