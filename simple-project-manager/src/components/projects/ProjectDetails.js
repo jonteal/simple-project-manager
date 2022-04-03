63af397b47a1d6bffb16babaaa2d504a00df6f14
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-03 13:58:36 -0500

    Created a Project Details component and nested the router path in App.js

---
+++ simple-project-manager/src/components/projects/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ProjectDetails = () => {
+    return (
+        <div className="container section project-details">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-title">Project Title</span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis blanditiis autem recusandae similique asperiores sed, ratione nemo ducimus voluptate explicabo aliquid, ipsa accusamus impedit consectetur quo accusantium vero praesentium numquam!</p>
+                </div>
+                <div className="card-action grey lighten-4 grey-text">
+                    <div>Posted by Jon Jackson</div>
+                    <div>2nd September, 2am</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectDetails;
