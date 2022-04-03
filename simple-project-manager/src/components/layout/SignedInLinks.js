7b81831b5b34351c8dd5af37ae9e2ab2b4143e53
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-02 23:40:40 -0500

    SignedInLinks component built and nested in navbar

--- simple-project-manager/src/components/layout/SignedInLinks.js
+++ simple-project-manager/src/components/layout/SignedInLinks.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const SignedInLinks = () => {
     return(
-        
+        <ul className="right">
+            <li><NavLink to='/'>New Project</NavLink></li>
+            <li><NavLink to='/'>Log Out</NavLink></li>
+            <li><NavLink to='/' className='btn btn-floating pink lighten-1'>JJ</NavLink></li>
+        </ul>
     )
 }
 
